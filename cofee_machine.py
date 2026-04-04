95bb84579b9986460f12749f64415a8ea27f1f44
Alen <kokkadalen@gmail.com>
2026-04-04 23:31:01 +0530

    feat: cofee machine in python

--- cofee_machine.py
+++ cofee_machine.py
@@ -1,15 +1,15 @@
-cofee_types = {
+COFEE_TYPES = {
     "Espresso": {"water": 50, "cofee": 18, "milk": 0, "price": 1.5},
     "Latte": {"water": 200, "cofee": 24, "milk": 150, "price": 2.5},
     "Cappuccino": {"water": 250, "cofee": 24, "milk": 100, "price": 3.0}
 }
 
-materials_in_machine = {"water": 300, "milk": 200, "cofee": 100, "cash":0}
+MATERIALS_IN_MACHINE = {"water": 300, "milk": 200, "cofee": 100, "cash":0}
 
 def resource_validation(cofee_type):
-    for item in cofee_types[cofee_type]:
+    for item in COFEE_TYPES[cofee_type]:
         if item != 'price':
-            if materials_in_machine[item] < cofee_types[cofee_type][item]:
+            if MATERIALS_IN_MACHINE[item] < COFEE_TYPES[cofee_type][item]:
                 print(f"Sorry, not enough {item}")
                 return False
     return True
@@ -24,28 +24,28 @@ def process_coin():
 
 def prepare_cofee(cofee_type):
     if resource_validation(cofee_type):
-        print(f"Cost for {cofee_type} is ${cofee_types[cofee_type]['price']}")
+        print(f"Cost for {cofee_type} is ${COFEE_TYPES[cofee_type]['price']}")
         recieved_money = process_coin()
-        if recieved_money < cofee_types[cofee_type]['price']:
-            print(f"Insufficient Money for '{cofee_type}'. Need ${cofee_types[cofee_type]['price'] - recieved_money}")
+        if recieved_money < COFEE_TYPES[cofee_type]['price']:
+            print(f"Insufficient Money for '{cofee_type}'. Need ${COFEE_TYPES[cofee_type]['price'] - recieved_money}")
         else:
-            change = recieved_money - cofee_types[cofee_type]["price"]
+            change = recieved_money - COFEE_TYPES[cofee_type]["price"]
             print(f"Here is your '{cofee_type}' and here is your change of ${round(change, 2)}.")
             update_resources(cofee_type)
     else:
         print("Sorry, that's not enough money. Money refunded.")
 
 def update_resources(cofee_type):
-    recipe = cofee_types[cofee_type]
+    recipe = COFEE_TYPES[cofee_type]
     for item in recipe:
         if item != 'price':
-            materials_in_machine[item] -= recipe[item]
-    materials_in_machine["cash"] += recipe["price"]
+            MATERIALS_IN_MACHINE[item] -= recipe[item]
+    MATERIALS_IN_MACHINE["cash"] += recipe["price"]
 
 def report():
     print("Cofee machine has below levels of raw ingredients")
-    for item in materials_in_machine:
-        print(f"{item}: {materials_in_machine[item]}")
+    for item in MATERIALS_IN_MACHINE:
+        print(f"{item}: {MATERIALS_IN_MACHINE[item]}")
     
 def main():
     print("Good day")
@@ -70,9 +70,9 @@ def main():
                 is_on = False
                 break
             case 'f':
-                for item in materials_in_machine:
+                for item in MATERIALS_IN_MACHINE:
                     if item != "cash":
-                        materials_in_machine[item] = 500 # Set back to max levels
+                        MATERIALS_IN_MACHINE[item] = 500 # Set back to max levels
                         print("Machine refilled!")
             case _:
                 print("Invalid Choice")
